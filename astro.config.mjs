ee064cd5aa5997d1d2ae7a497f1fbb434a700a15
me11is <martin.ryanellis@gmail.com>
2026-09-12 14:45:41 +0100

    finals

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@
 import { defineConfig, fontProviders } from 'astro/config';
 
 export default defineConfig({
+
+  devToolbar: {
+    enabled: false
+  },
   fonts: [
   {
     provider: fontProviders.local(),
